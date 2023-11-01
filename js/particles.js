0fa3d52eed35556ba529ced8064cf616f1a648e9
SRAB-9 <89607329+SRAB-9@users.noreply.github.com>
2023-11-01 14:22:43 +0000

    Add files via upload

---
+++ js/particles.js
@@ -0,0 +1,36 @@
+particlesJS("particles-js", {
+    "particles": {
+        "number": {
+            "value": 200,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": "#ffffff"
+        },
+        "shape": {
+            "type": "circle"
+        },
+        "opacity": {
+            "value": 0.5,
+            "random": true
+        },
+        "size": {
+            "value": 3
+        },
+        "move": {
+            "enable": true,
+            "speed": 0.5
+        }
+    },
+    "interactivity": {
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "repulse"
+            }
+        }
+    }
+});
